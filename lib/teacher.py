f917258ef70ad3befef467da05b14a440d4dde1e
Mercy <owinomercy983@gmail.com>
2025-05-22 08:14:43 +0300

    super

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Python basics",
+            "OOP principles",
+            "Inheritance in Python",
+            "How to write tests",
+            "Debugging techniques"
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
